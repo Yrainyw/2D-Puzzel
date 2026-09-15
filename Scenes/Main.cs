4c716a68db817af52fa44c0c4e4549acd2dbd790
Yrainy0419 <lily.tan12345678910@gmail.com>
2026-09-15 16:23:48 +0800

    Introduction to Signals

--- Scenes/Main.cs
+++ Scenes/Main.cs
@@ -5,20 +5,25 @@ namespace Game;
 
 public partial class Main : Node2D
 {
-	private Sprite2D sprite;
+	private Sprite2D cursor;
 	private PackedScene buildingScene;
+	private Button placeBuildingButton;
 
 	public override void _Ready()
 	{
 		buildingScene = GD.Load<PackedScene>("res://Scenes/Building/Building.tscn");
-		sprite = GetNode<Sprite2D>("Cursor");
+		cursor = GetNode<Sprite2D>("Cursor");
+		placeBuildingButton = GetNode<Button>("PlaceBuildingButton");
+		cursor.Visible = false;
+		placeBuildingButton.Pressed += OnButtonPressed;
 	}
 
     public override void _UnhandledInput(InputEvent evt)
     {
-        if (evt.IsActionPressed("left_click"))
+        if (cursor.Visible && evt.IsActionPressed("left_click"))
 		{
 			PlaceBuildingAtMousePosition();
+			cursor.Visible = false;
 		}
     }
 
@@ -26,7 +31,8 @@ public partial class Main : Node2D
 	public override void _Process(double delta)
 	{
 		var grid_position = GetMouseGridCellPosition();
-		sprite.GlobalPosition = grid_position * 64;
+
+		cursor.GlobalPosition = grid_position * 64;
 	}
 
 	private Vector2 GetMouseGridCellPosition()
@@ -41,10 +47,16 @@ public partial class Main : Node2D
 	private void PlaceBuildingAtMousePosition()
 	{
 		var building = buildingScene.Instantiate<Node2D>();
+
 		AddChild(building);
 
 		var grid_position = GetMouseGridCellPosition();
 
 		building.GlobalPosition = grid_position * 64;
+	}
+
+	private void OnButtonPressed()
+	{
+		cursor.Visible = true;
 	}
 }
