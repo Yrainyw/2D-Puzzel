445a854ec8a75e9f581369daa8d8c0b54aee76e5
Yrainy0419 <lily.tan12345678910@gmail.com>
2026-09-18 15:48:18 +0800

    Accounting for Trees in GridManager

--- Scenes/Manager/GridManager.cs
+++ Scenes/Manager/GridManager.cs
@@ -15,18 +15,23 @@ public partial class GridManager : Node
 	[Export]
 	private TileMapLayer baseTerrainTilemapLayer;
 
+	private List<TileMapLayer> allTilemapLayers;
+
 	public override void _Ready()
 	{
 		GameEvents.Instance.BuildingPlaced += OnBuildingPlaced;
+		allTilemapLayers = GetAllTilemapLayers(baseTerrainTilemapLayer);
 	}
 
 	public bool IsTilePositionValid(Vector2I tilePosition)
 	{
-		var customData = baseTerrainTilemapLayer.GetCellTileData(tilePosition);
-
-		if (customData == null) return false;
-
-		return (bool)customData.GetCustomData("Buildable");
+		foreach (var layer in allTilemapLayers)
+		{
+			var customData = layer.GetCellTileData(tilePosition);
+			if (customData == null) continue;
+			return (bool)customData.GetCustomData("Buildable");
+		}
+		return false;
 	}
 
 	public bool IsTilePositionBuildable(Vector2I tilePosition)
@@ -69,6 +74,22 @@ public partial class GridManager : Node
 
 		gridPosition = gridPosition.Floor();
 		return new Vector2I((int)gridPosition.X, (int)gridPosition.Y);
+	}
+
+	private List<TileMapLayer> GetAllTilemapLayers(TileMapLayer rootTileMapLayer)
+	{
+		var result = new List<TileMapLayer>();
+		var children = rootTileMapLayer.GetChildren();
+		children.Reverse();
+
+		foreach (var child in children){
+			if (child is TileMapLayer childLayer)
+			{
+				result.AddRange(GetAllTilemapLayers(childLayer));
+			}
+		}
+		result.Add(rootTileMapLayer);
+		return result;
 	}
 
 	private void UpdateValidBuildableTiles(BuildingComponent buildingComponent)
